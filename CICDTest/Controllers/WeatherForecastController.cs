2960e7cb427f590009d1b48f2bb07f0d1ba2c591
rehdak225 <88677681+rehdak225@users.noreply.github.com>
2023-12-22 17:46:15 +0000

    test ci

--- CICDTest/Controllers/WeatherForecastController.cs
+++ CICDTest/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace CICDTest.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var hey = "";
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
